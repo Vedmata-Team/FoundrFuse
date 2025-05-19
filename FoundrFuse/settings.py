20ff0764a1f003b17ebe7d8988d14f4a78c983fe
divy-mohan <divymohan.awgp@gmail.com>
2025-05-19 13:16:29 +0530

    Updated settings for template

--- FoundrFuse/settings.py
+++ FoundrFuse/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'FoundrFuse.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+         'DIRS': [BASE_DIR / 'templates'],  # ✅ Added this line
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,6 +115,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # ✅ Add this line
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
