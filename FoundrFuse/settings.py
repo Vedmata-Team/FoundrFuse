b91b58b32ad04cee31070e368fad5e7c4e6be137
divy-mohan <vedmatawebdesigning@gmail.com>
2025-07-10 13:02:58 +0530

    modified .env

--- FoundrFuse/settings.py
+++ FoundrFuse/settings.py
@@ -6,7 +6,7 @@ load_dotenv()  # Add this near the top of the file
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-foundrfuse-dev-key')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
